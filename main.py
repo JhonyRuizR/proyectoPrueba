74dc29d95ca916d8463fe796a0246c5c58da65ec
Jhony <roy.ruiz.27@unsch.edu.pe>
2021-12-05 10:35:58 -0500

    Modificando la funcion run e importando el modulo user

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import user
 def run():
     usuario = input("Ingrese su Usuario:\n")
     clave = input("Ingrese su Clave:\n")
 
-    if usuario == "jhony" and clave == "7981":
+    datos = user.datos_usuario()
+    if usuario == datos[0] and clave == datos[1]:
         print("Usuario logueado correctamente")
     else:
         print("Usuario o clave Incorrecta!!!")
