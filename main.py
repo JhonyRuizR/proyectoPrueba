50851f5ebe71f734efe9e9f374067f9949e1e55c
Jhony <roy.ruiz.27@unsch.edu.pe>
2021-12-05 10:19:24 -0500

    Modificando la funcion run

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def run():
-    pass
+    usuario = input("Ingrese su Usuario:\n")
+    clave = input("Ingrese su Clave:\n")
+
+    if usuario == "jhony" and clave == "7981":
+        print("Usuario logueado correctamente")
+    else:
+        print("Usuario o clave Incorrecta!!!")
 
 
 if __name__ == "__main__":
